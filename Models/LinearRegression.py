a965e0ddf9b919599178b0009dfe4704d250f406
Abzhik <52734118+Abjik@users.noreply.github.com>
2024-03-25 22:10:32 +0500

    finish with lr

--- Models/LinearRegression.py
+++ Models/LinearRegression.py
@@ -1,104 +1,51 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error
-from typing import Optional
 
-class MyLinearRegression:
-    def __init__(self, fit_intercept: bool = True, lr: float = 0.01, max_iter: int = 100, sgd: bool = False, n_sample: int = 16):
-        """
-        Initialize the MyLinearRegression class.
-
-        Parameters:
-        fit_intercept (bool): Whether to calculate the intercept for this model. If set to False, no intercept will be used in calculations.
-        lr (float): The learning rate.
-        max_iter (int): The maximum number of iterations.
-        sgd (bool): Whether to use Stochastic Gradient Descent.
-        n_sample (int): The number of samples to use for Stochastic Gradient Descent.
-        """
-        self.fit_intercept = fit_intercept
-        self.w = None
+class MyDailyLinearRegression:
+    def __init__(self, lr=0.01, max_iter=100, sgd=False, n_sample=30):
         self.lr = lr
         self.max_iter = max_iter
         self.sgd = sgd
         self.n_sample = n_sample
+        self.coef_ = None
+        self.intercept_ = None
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> 'MyLinearRegression':
-        """
-        Fit the model according to the given training data.
+    def fit(self, X, y):
+        # Добавляем столбец из единиц, чтобы учесть коэффициент смещения (intercept)
+        X = np.c_[X, np.ones(X.shape[0])]
+        
+        # Вычисляем параметры модели аналитически используя метод наименьших квадратов
+        self.coef_ = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.intercept_ = self.coef_[-1]
+        self.coef_ = self.coef_[:-1]
 
-        Parameters:
-        X (np.ndarray): Training data.
-        y (np.ndarray): Target values.
+    def predict(self, X):
+        # Добавляем столбец из единиц для учета коэффициента смещения (intercept)
+        X = np.c_[X, np.ones(X.shape[0])]
+        
+        # Вычисляем предсказанные значения
+        return X @ np.append(self.coef_, self.intercept_)
+    
+class MyDailyLinearRegressionRamadan:
+    def __init__(self, lr, max_iter, sgd, n_sample):
+        self.lr = lr
+        self.max_iter = max_iter
+        self.sgd = sgd
+        self.n_sample = n_sample
+        self.coef_ = None
+        self.intercept_ = None
 
-        Returns:
-        self: returns an instance of self.
-        """
-        n, k = X.shape
+    def fit(self, X, y, ramadan, covid):
+        # Добавляем столбец из единиц, чтобы учесть коэффициент смещения (intercept)
+        X = np.c_[X, np.ones(X.shape[0])]
+        
+        # Вычисляем параметры модели аналитически используя метод наименьших квадратов
+        self.coef_ = np.linalg.inv(X.T @ X) @ X.T @ y
+        self.intercept_ = self.coef_[-1]
+        self.coef_ = self.coef_[:-1]
 
-        if self.w is None:
-            self.w = np.random.randn(k + 1 if self.fit_intercept else k)
-
-        X_train = np.hstack((X, np.ones((n, 1)))) if self.fit_intercept else X
-
-        self.losses = []
-
-        for _ in range(self.max_iter):
-            y_pred = self.predict(X)
-            self.losses.append(mean_squared_error(y_pred, y))
-
-            grad = self.__calc_gradient(X_train, y, y_pred)
-
-            assert grad.shape == self.w.shape, f"gradient shape {grad.shape} is not equal weight shape {self.w.shape}"
-            self.w -= self.lr * grad
-
-        return self
-
-    def __calc_gradient(self, X: np.ndarray, y: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-        """
-        Calculate the gradient.
-
-        Parameters:
-        X (np.ndarray): Training data.
-        y (np.ndarray): Target values.
-        y_pred (np.ndarray): Predicted values.
-
-        Returns:
-        grad (np.ndarray): The calculated gradient.
-        """
-        if self.sgd:
-            inds = np.random.choice(np.arange(X.shape[0]), size=self.n_sample, replace=False)
-            grad = 2 * (y_pred[inds] - y[inds])[:, np.newaxis] * X[inds]
-        else:
-            grad = 2 * (y_pred - y)[:, np.newaxis] * X
-        return grad.mean(axis=0)
-
-    def get_losses(self) -> list:
-        """
-        Get the losses.
-
-        Returns:
-        losses (list): The losses.
-        """
-        return self.losses
-
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        """
-        Predict using the linear model.
-
-        Parameters:
-        X (np.ndarray): Samples.
-
-        Returns:
-        y_pred (np.ndarray): Returns predicted values.
-        """
-        n, k = X.shape
-        X_train = np.hstack((X, np.ones((n, 1)))) if self.fit_intercept else X
-        return X_train @ self.w
-
-    def get_weights(self) -> Optional[np.ndarray]:
-        """
-        Get the weights.
-
-        Returns:
-        w (np.ndarray): Returns the weights.
-        """
-        return self.w
+    def predict(self, X):
+        # Добавляем столбец из единиц для учета коэффициента смещения (intercept)
+        X = np.c_[X, np.ones(X.shape[0])]
+        
+        # Вычисляем предсказанные значения
+        return X @ np.append(self.coef_, self.intercept_)
